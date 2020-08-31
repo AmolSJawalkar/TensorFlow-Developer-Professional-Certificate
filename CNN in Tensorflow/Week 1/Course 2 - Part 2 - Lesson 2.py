b9c93ccdfb7032cd54821a9a7cb4554eb23c7a95
Amol Jawalkar <jawalkaramol12@gmail.com>
2020-08-31 13:30:32 +0530

    Added data for CNN in Tensorflow for week 1

---
+++ CNN in Tensorflow/Week 1/Course 2 - Part 2 - Lesson 2.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# Below code is to  Disable all GPUS as it take much time on my PC to detect it
+tf.config.set_visible_devices([], 'GPU')
+
