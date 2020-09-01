39c6ca0ebf779fa94bfb6c1dbbb1d1c71c9df94d
Amol Jawalkar <jawalkaramol12@gmail.com>
2020-09-01 22:53:29 +0530

    Completed RPS lesson for week 4

---
+++ CNN in Tensorflow/Week 4/RPS.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+tf.config.set_visible_devices([], 'GPU')
+
+inceptionModel = tf.keras.applications.inception_v3.InceptionV3(include_top=False, input_shape=(150, 150, 3), weights=None)
+inceptionModel.load_weights('../data/weights/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5')
+for layer in inceptionModel.layers:
+    layer.trainable = False
+
+lastlayer = inceptionModel.get_layer('mixed10')
+
+x = tf.keras.layers.Flatten()(lastlayer.output)
+x = tf.keras.layers.Dense(units=512, activation='relu')(x)
+x = tf.keras.layers.Dense(units=3, activation='softmax')(x)
+
+ourmodel = tf.keras.models.Model(inputs=inceptionModel.inputs, outputs=x)
+ourmodel.compile(loss=tf.keras.losses.categorical_crossentropy, metrics=['acc'])
+
+train_DataGenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./ 255,
+    rotation_range=40,
+    height_shift_range=0.2,
+    width_shift_range=0.2,
+    shear_range=0.2, zoom_range=0.2,
+    horizontal_flip=True
+)
+
+valid_DataGenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./ 255
+)
+
+sourcedir = '../data/rps'
+trainImageDataGenerator = train_DataGenerator.flow_from_directory(
+    sourcedir + '/rps',
+    target_size=(150, 150),
+    class_mode='categorical',
+    batch_size=20
+)
+
+validImageDataGenerator = train_DataGenerator.flow_from_directory(
+    sourcedir + '/rps-test-set',
+    target_size=(150, 150),
+    class_mode='categorical',
+    batch_size=20
+)
+
+history = ourmodel.fit(trainImageDataGenerator, epochs=10, validation_data=validImageDataGenerator)
