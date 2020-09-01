3b8651430726c6a363cbb057a9cba162d714ecc5
Amol Jawalkar <jawalkaramol12@gmail.com>
2020-09-01 11:25:28 +0530

    add logic to see the convolution layers out put

--- CNN in Tensorflow/Week 1/Course 2 - Part 2 - Lesson 2.py
+++ CNN in Tensorflow/Week 1/Course 2 - Part 2 - Lesson 2.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Below code is to  Disable all GPUS as it take much time on my PC to detect it
 tf.config.set_visible_devices([], 'GPU')
@@ -40,7 +41,7 @@ validDataGenerator = imageDataGenerator.flow_from_directory(
 # Train the model
 history = model.fit(trainDataGenerator,
                     steps_per_epoch=100,
-                    epochs=10,
+                    epochs=5,
                     validation_data=validDataGenerator,
                     validation_steps=50)
 
@@ -62,6 +63,35 @@ plt.plot(epochs, history.history['val_loss'])
 plt.title("Training and validation loss")
 plt.xlabel("Epoch")
 plt.ylabel("loss")
+
+# Visualize the Convolution layers outputs
+image = tf.keras.preprocessing.image.load_img(sourcedir + '/validation/dogs/dog.2039.jpg', target_size=(150, 150))
+imgarray = tf.keras.preprocessing.image.img_to_array(image)
+imgarray = imgarray.reshape(1, 150, 150, 3) / 255.0
+print(imgarray.shape)
+
+outputs = [layer.output for layer in model.layers]
+activation_model = tf.keras.models.Model(inputs=model.inputs, outputs=outputs)
+predictions = activation_model.predict(imgarray)
+for prection in predictions:
+    if len(prection.shape) == 4:
+        size = prection.shape[1]
+        channels = prection.shape[-1]
+        grid = np.zeros((size, size * channels))
+        for channelNo in range(channels):
+            x = prection[0, :, :, channelNo]
+            x -= x.mean()
+            x /= x.std()
+            x *= 64
+            x += 128
+            x = np.clip(x, 0, 255).astype('uint8')
+            grid[:, channelNo * size: (channelNo + 1) * size] = x
+
+        scale = 20. / channels
+        figsize = (scale * channels, scale)
+        print(figsize)
+        plt.figure(figsize=figsize)
+        plt.imshow(grid)
 
 
 
